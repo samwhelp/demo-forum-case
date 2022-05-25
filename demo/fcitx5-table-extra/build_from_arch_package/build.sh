481654c703a3ffe8c41c0d149d12c7acf3bebe03
samwhelp <samwhelp@users.noreply.github.com>
2022-05-25 16:41:26 +0800

    fix

--- demo/fcitx5-table-extra/build_from_arch_package/build.sh
+++ demo/fcitx5-table-extra/build_from_arch_package/build.sh
@@ -1,30 +1,41 @@
 #!/usr/bin/env bash
 
+
 PKG_ROOT="fcitx5-table-extra"
+
+
+
 
 mkdir -p "var/deb/$PKG_ROOT"
 
-rm -rf "var/deb/$PKG_ROOT/usr"
+rm -rf var/deb/$PKG_ROOT/*
+
+
+
 
 cp -av "var/src/usr" "var/deb/$PKG_ROOT/usr"
 
+
+
+
 mkdir -p "var/deb/$PKG_ROOT/DEBIAN"
-
-
 
 cat > "var/deb/$PKG_ROOT/DEBIAN/control" << EOF
 Package: fcitx5-table-extra
-Version: 0.1
+Version: 0.1.0
 Architecture: all
 Description: This is a personal package which build from arch package.
-Maintainer: developer <developer@home>
+Maintainer: developer <developer@home.work>
 
 EOF
 
 
-cd 'var/deb'
 
-rm -f ./*.deb
+
+rm -f var/*.deb
+
+
+cd 'var/deb'
 
 dpkg -b "$PKG_ROOT"
 
