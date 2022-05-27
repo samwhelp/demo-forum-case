e5600c42b7f4058818749f17923e485eace83d3f
samwhelp <samwhelp@users.noreply.github.com>
2022-05-28 02:49:13 +0800

    fix

---
+++ demo/fcitx5-table-extra/build_deb_from_github_source_by_way4/fcitx5-config.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+install -Dm644 "./config/fcitx5/profile" "$HOME/.config/fcitx5/profile"
---
+++ demo/fcitx5-table-extra/build_deb_from_github_source_by_way4/prepare.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+sudo apt-get install dpkg-dev wget zstd devscripts debhelper git cmake extra-cmake-modules libimecore-dev libimetable-dev libboost-dev libfcitx5core-dev libfcitx5utils-dev
+
+
+# libboost-all-dev
+# libboost-dev
+
