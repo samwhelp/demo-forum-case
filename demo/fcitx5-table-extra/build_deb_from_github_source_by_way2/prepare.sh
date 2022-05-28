36d84b6972d1bbe757b0da4a482682a84ecf79ce
samwhelp <samwhelp@users.noreply.github.com>
2022-05-28 09:46:15 +0800

    fix

--- demo/fcitx5-table-extra/build_deb_from_github_source_by_way2/prepare.sh
+++ demo/fcitx5-table-extra/build_deb_from_github_source_by_way2/prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-sudo apt-get install git wget zstd build-essential dpkg-dev devscripts debhelper cmake extra-cmake-modules libboost-dev libimecore-dev libfcitx5core-dev libfcitx5utils-dev libimetable-dev
+sudo apt-get install git wget zstd build-essential dpkg-dev devscripts debhelper cmake extra-cmake-modules libboost-dev libimecore-dev libimetable-dev libfcitx5core-dev libfcitx5utils-dev
 
 
 # libboost-all-dev
