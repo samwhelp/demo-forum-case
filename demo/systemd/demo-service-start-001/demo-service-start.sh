611060b75e33f162e0419f377e839fdb30b60528
samwhelp <samwhelp@users.noreply.github.com>
2022-08-04 16:38:16 +0800

    fix

---
+++ demo/systemd/demo-service-start-001/demo-service-start.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo
+echo "This is demo sevice start message!"
+echo
+echo "Press [CTRL + C] to exit!"
+echo
+
+while true; do
+	echo "Tick: $(date +"%Y-%m-%d %H:%M:%S")"
+
+	#sleep 1m
+	#sleep 0.5m
+	sleep 10s
+	#sleep 1s
+done
+
+
+##
+## ## Help
+##
+## $ man sleep
+## $ help while
+##
