86e2ddeff63ab5d83e0d02e6539a9e05b06d4630
samwhelp <samwhelp@users.noreply.github.com>
2023-04-03 18:17:32 +0800

    fix

---
+++ demo/fcitx5-mcbopomofo/build_deb_from_github_source_by_way_003/build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+
+THE_PRJ_NAME="fcitx5-mcbopomofo"
+
+THE_PKG_NAME="${THE_PRJ_NAME}"
+
+THE_PKG_VERSION="0.1.1"
+
+
+mkdir -p "var/src/${THE_PRJ_NAME}/build/pkg-root"
+
+
+
+
+rm -rf "var/deb"
+
+mkdir -p "var/deb/${THE_PKG_NAME}"
+
+
+
+
+cp -rfv "var/src/${THE_PRJ_NAME}/." "var/deb/${THE_PKG_NAME}"
+
+cp -rfv "asset/build/debian/." "var/deb/${THE_PKG_NAME}/debian"
+
+
+
+
+cd "var/deb/${THE_PKG_NAME}"
+
+#debchange --create --package "${THE_PKG_NAME}" --newversion "${THE_PKG_VERSION}" --controlmaint --distribution "unstable" --urgency "low" "Init Release" --changelog debian/changelog
+
+debchange --newversion "${THE_PKG_VERSION}" --controlmaint --distribution "unstable" --urgency "low" "Latest Release" --changelog debian/changelog
+
+
+debuild -i -us -uc
+
+cd "${OLDPWD}"
---
+++ demo/fcitx5-mcbopomofo/build_deb_from_github_source_by_way_003/fcitx5-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+sudo apt-get install \
+	fcitx5 \
+	fcitx5-config-qt \
+	fcitx5-frontend-gtk2 \
+	fcitx5-frontend-gtk3 \
+	fcitx5-frontend-gtk4 \
+	fcitx5-frontend-qt5
